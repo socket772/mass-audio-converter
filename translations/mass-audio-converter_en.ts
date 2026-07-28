76af5661ea17e33e2b3172b04ca62e369fcae780
socket772 <socket772@protonmail.com>
2026-07-29 00:32:27 +0200

    Aggiunte prime traduzioni

--- translations/mass-audio-converter_en.ts
+++ translations/mass-audio-converter_en.ts
@@ -1,47 +1,42 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en" sourcelanguage="en">
+<TS version="2.1" language="en_US" sourcelanguage="en_US">
 <context>
     <name>MainWindow</name>
     <message>
         <location filename="../mainwindow.ui" line="85"/>
-        <source>output</source>
-        <translation type="unfinished"></translation>
+        <source>output_folder button</source>
+        <translation type="unfinished">Output folder</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="101"/>
         <source>overwrite_checkbox_text</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Overwrite?</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="145"/>
         <source>format_label</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Audio format</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="152"/>
-        <source>input</source>
-        <translation type="unfinished"></translation>
+        <source>input_folder_button</source>
+        <translation type="unfinished">Input folder</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="194"/>
         <source>threads_label</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="216"/>
-        <source>auto</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Threads number</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="233"/>
         <source>progress_bar_label</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Progress</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="39"/>
         <source>file_picker_select_folder</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Select a folder</translation>
     </message>
 </context>
 </TS>
--- translations/mass-audio-converter_it.ts
+++ translations/mass-audio-converter_it.ts
@@ -1,47 +1,42 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="it" sourcelanguage="en">
+<TS version="2.1" language="it_IT" sourcelanguage="en_US">
 <context>
     <name>MainWindow</name>
     <message>
         <location filename="../mainwindow.ui" line="85"/>
-        <source>output</source>
-        <translation type="unfinished"></translation>
+        <source>output_folder button</source>
+        <translation type="unfinished">Cartella output</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="101"/>
         <source>overwrite_checkbox_text</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Sovrascrivi?</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="145"/>
         <source>format_label</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Formato audio</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="152"/>
-        <source>input</source>
-        <translation type="unfinished"></translation>
+        <source>input_folder_button</source>
+        <translation type="unfinished">Cartella input</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="194"/>
         <source>threads_label</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="216"/>
-        <source>auto</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Lavoratori paralleli</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="233"/>
         <source>progress_bar_label</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Progresso</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="39"/>
         <source>file_picker_select_folder</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Seleziona una cartella</translation>
     </message>
 </context>
 </TS>
